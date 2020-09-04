2e865c6baf7e5f819ac4ca69095f52f224fa9bd8
Sam Herrmann <samherrmann.github@gmail.com>
2020-09-04 10:11:57 -0400

    Rename Expect to Expected

--- flag/parse_test.go
+++ flag/parse_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Tests []struct {
-	Args   []string
-	Expect *Expect
+	Args     []string
+	Expected *Expected
 }
 
-type Expect struct {
+type Expected struct {
 	Config *Config
 	Err    error
 }
@@ -20,43 +20,43 @@ func TestParse(t *testing.T) {
 	tests := Tests{
 		{
 			Args: []string{},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: &Config{Port: 8080, NotFoundFile: ""},
 				Err:    nil,
 			},
 		}, {
 			Args: []string{"-port", "3000"},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: &Config{Port: 3000, NotFoundFile: ""},
 				Err:    nil,
 			},
 		}, {
 			Args: []string{"-port", "foo"},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: nil,
 				Err:    errors.New(""),
 			},
 		}, {
 			Args: []string{"-not-found-file"},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: nil,
 				Err:    errors.New(""),
 			},
 		}, {
 			Args: []string{"-not-found-file", "404.html"},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: &Config{Port: 8080, NotFoundFile: "404.html"},
 				Err:    nil,
 			},
 		}, {
 			Args: []string{"-not-found-file", "foo"},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: &Config{Port: 8080, NotFoundFile: "foo"},
 				Err:    nil,
 			},
 		}, {
 			Args: []string{"-port", "3000", "-not-found-file", "index.html"},
-			Expect: &Expect{
+			Expected: &Expected{
 				Config: &Config{Port: 3000, NotFoundFile: "index.html"},
 				Err:    nil,
 			},
@@ -66,25 +66,25 @@ func TestParse(t *testing.T) {
 	for _, tc := range tests {
 		got, err := parse(tc.Args)
 		// Check if an error occured when no error is expected
-		if err != nil && tc.Expect.Err == nil {
+		if err != nil && tc.Expected.Err == nil {
 			t.Errorf("For arguments %+v, an error occured but no error was expected.", tc.Args)
 			continue
 		}
 		// Check if no error occured when one is expected
-		if err == nil && tc.Expect.Err != nil {
+		if err == nil && tc.Expected.Err != nil {
 			t.Errorf("For arguments %+v, expected an error but no error occured.", tc.Args)
 			continue
 		}
 		// If error occured when one is expected, then all is well. Exit before checking values.
-		if err != nil && tc.Expect.Err != nil {
+		if err != nil && tc.Expected.Err != nil {
 			continue
 		}
 		// Check values against Expected values
-		if got.Port != tc.Expect.Config.Port || got.NotFoundFile != tc.Expect.Config.NotFoundFile {
+		if got.Port != tc.Expected.Config.Port || got.NotFoundFile != tc.Expected.Config.NotFoundFile {
 			t.Errorf(
 				"For arguments %+v, expected %+v, but got %+v.",
 				tc.Args,
-				*tc.Expect.Config,
+				*tc.Expected.Config,
 				*got,
 			)
 		}
