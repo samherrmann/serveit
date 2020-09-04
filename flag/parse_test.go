587fb8a869e0bd9270c08ef8208201c7a67704fb
Sam Herrmann <samherrmann.github@gmail.com>
2020-09-04 10:43:12 -0400

    Make test types local to test

--- flag/parse_test.go
+++ flag/parse_test.go
@@ -5,87 +5,87 @@ import (
 	"testing"
 )
 
-type Tests []struct {
-	Args     []string
-	Expected *Expected
-}
-
-type Expected struct {
-	Config *Config
-	Err    error
-}
-
 func TestParse(t *testing.T) {
+	type Want struct {
+		config *Config
+		err    error
+	}
+
+	type Test struct {
+		args []string
+		want *Want
+	}
+
 	// Define test cases
-	tests := Tests{
+	tests := []Test{
 		{
-			Args: []string{},
-			Expected: &Expected{
-				Config: &Config{Port: 8080, NotFoundFile: ""},
-				Err:    nil,
+			args: []string{},
+			want: &Want{
+				config: &Config{Port: 8080, NotFoundFile: ""},
+				err:    nil,
 			},
 		}, {
-			Args: []string{"-port", "3000"},
-			Expected: &Expected{
-				Config: &Config{Port: 3000, NotFoundFile: ""},
-				Err:    nil,
+			args: []string{"-port", "3000"},
+			want: &Want{
+				config: &Config{Port: 3000, NotFoundFile: ""},
+				err:    nil,
 			},
 		}, {
-			Args: []string{"-port", "foo"},
-			Expected: &Expected{
-				Config: nil,
-				Err:    errors.New(""),
+			args: []string{"-port", "foo"},
+			want: &Want{
+				config: nil,
+				err:    errors.New(""),
 			},
 		}, {
-			Args: []string{"-not-found-file"},
-			Expected: &Expected{
-				Config: nil,
-				Err:    errors.New(""),
+			args: []string{"-not-found-file"},
+			want: &Want{
+				config: nil,
+				err:    errors.New(""),
 			},
 		}, {
-			Args: []string{"-not-found-file", "404.html"},
-			Expected: &Expected{
-				Config: &Config{Port: 8080, NotFoundFile: "404.html"},
-				Err:    nil,
+			args: []string{"-not-found-file", "404.html"},
+			want: &Want{
+				config: &Config{Port: 8080, NotFoundFile: "404.html"},
+				err:    nil,
 			},
 		}, {
-			Args: []string{"-not-found-file", "foo"},
-			Expected: &Expected{
-				Config: &Config{Port: 8080, NotFoundFile: "foo"},
-				Err:    nil,
+			args: []string{"-not-found-file", "foo"},
+			want: &Want{
+				config: &Config{Port: 8080, NotFoundFile: "foo"},
+				err:    nil,
 			},
 		}, {
-			Args: []string{"-port", "3000", "-not-found-file", "index.html"},
-			Expected: &Expected{
-				Config: &Config{Port: 3000, NotFoundFile: "index.html"},
-				Err:    nil,
+			args: []string{"-port", "3000", "-not-found-file", "index.html"},
+			want: &Want{
+				config: &Config{Port: 3000, NotFoundFile: "index.html"},
+				err:    nil,
 			},
 		},
 	}
 	// Loop over all test cases
 	for _, tc := range tests {
-		got, err := parse(tc.Args)
+		got, err := parse(tc.args)
 		// Check if an error occured when no error is expected
-		if err != nil && tc.Expected.Err == nil {
-			t.Errorf("For arguments %+v, an error occured but no error was expected.", tc.Args)
+		if err != nil && tc.want.err == nil {
+			t.Errorf("For arguments %+v, want no error but got an error.", tc.args)
 			continue
 		}
 		// Check if no error occured when one is expected
-		if err == nil && tc.Expected.Err != nil {
-			t.Errorf("For arguments %+v, expected an error but no error occured.", tc.Args)
+		if err == nil && tc.want.err != nil {
+			t.Errorf("For arguments %+v, want an error but no error occured.", tc.args)
 			continue
 		}
 		// If error occured when one is expected, then all is well. Exit before checking values.
-		if err != nil && tc.Expected.Err != nil {
+		if err != nil && tc.want.err != nil {
 			continue
 		}
 		// Check values against Expected values
-		if got.Port != tc.Expected.Config.Port || got.NotFoundFile != tc.Expected.Config.NotFoundFile {
+		if got.Port != tc.want.config.Port || got.NotFoundFile != tc.want.config.NotFoundFile {
 			t.Errorf(
-				"For arguments %+v, expected %+v, but got %+v.",
-				tc.Args,
-				*tc.Expected.Config,
+				"For arguments %+v, got %+v, but want %+v.",
+				tc.args,
 				*got,
+				*tc.want.config,
 			)
 		}
 	}
